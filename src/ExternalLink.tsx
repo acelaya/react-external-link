8fa6afc26fd2c4a2d92e41861290edb172ac2a4f
Alejandro Celaya <alejandrocelaya@gmail.com>
2025-08-12 09:02:18 +0200

    Allow a ref to be passed to ExternalLink

--- src/ExternalLink.tsx
+++ src/ExternalLink.tsx
@@ -1,8 +1,9 @@
-import type { AnchorHTMLAttributes, FC } from 'react';
+import type { FC, HTMLAttributes, RefObject } from 'react';
 
-export interface ExternalLinkProps extends Omit<AnchorHTMLAttributes<Element>, 'href'> {
+export type ExternalLinkProps = Omit<HTMLAttributes<HTMLAnchorElement>, 'href'> & {
   href: string;
-}
+  ref?: RefObject<HTMLAnchorElement | null>;
+};
 
 export const ExternalLink: FC<ExternalLinkProps> = ({ href, children, rel, ...rest }: ExternalLinkProps) => (
   <a target="_blank" rel={`noopener noreferrer${rel ? ` ${rel}` : ''}`} href={href} {...rest}>
